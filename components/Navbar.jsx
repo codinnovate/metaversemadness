ca83b9ea4e0a8a4fa389fb86e9f9cc4248823112
cody <adeyemis710@gmail.com>
2023-10-12 00:53:13 +0100

    added

--- components/Navbar.jsx
+++ components/Navbar.jsx
@@ -1,7 +1,7 @@
 'use client';
 import { motion } from 'framer-motion';
-import styles from '../styles'
-import {navVariants} from '../utils/motion'
+import styles from '../styles';
+import { navVariants } from '../utils/motion';
 
 const Navbar = () => (
   <motion.nav
